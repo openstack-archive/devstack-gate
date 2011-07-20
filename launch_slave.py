c88ed7c9687da1820f7123a846f6dfbdae75abd0
Monty Taylor <mordred@inaugust.com>
2011-07-20 12:37:49 -0400

    Move the keys into the puppet dir for easy install.
    Change-Id: I6a3374a678e77a24b5c28a8c3a4d78a8bf06ca1c

--- launch_slave.py
+++ launch_slave.py
@@ -35,6 +35,7 @@ apt-get install -y --force-yes git rubygems
 gem install --no-rdoc --no-ri puppet
 git clone git://github.com/openstack/openstack-ci-puppet.git
 cd openstack-ci-puppet
+mv /root/slave_*_key modules/jenkins_slave/files/
 ln -sf /root/openstack-ci-puppet/manifests/%s.pp manifests/this.pp
 /var/lib/gems/1.8/bin/puppet apply -l /tmp/manifest.log --modulepath=`pwd`/modules manifests/this.pp
 """ % node_manifest)
