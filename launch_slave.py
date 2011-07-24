c62b369ddcddf27188f2c9e2d9b3e840b2241e42
Monty Taylor <mordred@inaugust.com>
2011-07-24 14:08:13 -0400

    Use getopt to process options - they're getting complex.
    Change-Id: I4ed418a9d636fec0f6ff433e447896a80d773d86

--- launch_slave.py
+++ launch_slave.py
@@ -2,19 +2,30 @@ from libcloud.types import Provider
 from libcloud.providers import get_driver
 from libcloud.deployment import MultiStepDeployment, ScriptDeployment, SSHKeyDeployment
 import os, sys
+import getopt
 
 CLOUD_SERVERS_USERNAME = os.environ['CLOUD_SERVERS_USERNAME']
 CLOUD_SERVERS_API_KEY = os.environ['CLOUD_SERVERS_API_KEY']
-try:
-  node_name = sys.argv[1]
-except:
+
+(option_pairs, args) = getopt.getopt(sys.argv[1:], [], ["distro="])
+
+if len(args) == 0:
   print "Node Name required!"
   sys.exit(1)
-
+node_name = args[0]
 
 node_manifest = "slave"
-if len(sys.argv) > 2:
-  node_manifest = sys.argv[2]
+node_type = '76'
+
+if len(args) > 1:
+  node_manifest = args[1]
+
+for (name, value) in option_pairs:
+  if name == "--distro":
+    if value == "maverick":
+      node_type = '69'
+    if value == "natty":
+      node_type = '76'
 
 files={}
 for key in ("slave_private_key", "slave_gpg_key", "slave_tarmac_key"):
@@ -47,7 +58,7 @@ msd = MultiStepDeployment([sd, script])
 images = conn.list_images()
 
 size = [sz for sz in conn.list_sizes() if sz.id == '3'][0]
-image = [img for img in conn.list_images() if img.id == '76'][0]
+image = [img for img in conn.list_images() if img.id == node_type][0]
 
 
 # deploy_node takes the same base keyword arguments as create_node.
