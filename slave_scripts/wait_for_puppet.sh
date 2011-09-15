4201578eae7f6e8ed0e10734d2781e9fa406135f
James E. Blair <james.blair@rackspace.com>
2011-09-15 08:59:08 -0700

    Fix logic problem with waiting for puppet.
    Change-Id: Ic1bfef5b98900206a525cb488d60fed942c15d05

--- slave_scripts/wait_for_puppet.sh
+++ slave_scripts/wait_for_puppet.sh
@@ -11,10 +11,10 @@ HOSTS=$@
 echo "Jenkins: Waiting for puppet to complete on all nodes"
 DONE=0
 while [ $DONE != 1 ]; do
-    DONE=0
+    DONE=1
     for hostname in $HOSTS
     do
-	if (grep "$hostname $FINISH_RE" $LOGFILE >/dev/null); then DONE=1; fi
+	if !(grep "$hostname $FINISH_RE" $LOGFILE >/dev/null); then DONE=0; fi
     done
     sleep 5
 done
