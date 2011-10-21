bc8435f684a3544fa714555a0bc14b2e7d566ded
Monty Taylor <mordred@inaugust.com>
2011-10-21 13:17:42 -0400

    Be specific about facter and puppet versions.
    Change-Id: I28cd9652fe565a8cbb6905aa84d5e7a1d7323b23

--- launch_slave.py
+++ launch_slave.py
@@ -59,7 +59,8 @@ launch_script = """perl -ple 's/main/main universe/' -i /etc/apt/sources.list
 apt-get update
 apt-get -y --force-yes upgrade
 apt-get install -y --force-yes git rubygems
-gem install --no-rdoc --no-ri puppet
+gem install --no-rdoc --no-ri --version=1.6.0 facter
+gem install --no-rdoc --no-ri --version=2.7.1 puppet
 cd /root
 git clone git://github.com/openstack/openstack-ci-puppet.git
 cd openstack-ci-puppet
