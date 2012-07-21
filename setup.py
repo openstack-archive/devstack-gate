393e2756444da28d7ffa6de475bb81eb76f6ca2d
Bhuvan Arumugam <bhuvan@apache.org>
2012-07-20 21:37:21 -0700

    Generate AUTHORS and ChangeLog for devstack-gate.
    Bug: 1027344
    
    Inline with changes in other projects, generate AUTHORS
    and ChangeLog to include in release package.
    
    * .gitignore
      Ignore these 2 files.
    
    * MANIFEST.in
      Include them in the package.
    
    * setup.py
      Command class to generate these files, from openstack.common.
    
    * devstackgate/openstack/common/setup.py
      Import from openstack.common project.
    
    * openstack-common.conf
      Configuration file to sync changes using the update script.
    
    Change-Id: Ia08d70600e012be7759a03f3afe21aa09b804262

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ import os
 import sys
 import setuptools
 
+from devstackgate.openstack.common import setup as common_setup
 
 setuptools.setup(
     name="devstack-gate",
     version="2012.2",
-    description="Devstack gate scripts used by Openstack CI team for testing longest line in this screen",
+    description="Devstack gate scripts used by Openstack CI team",
     url='https://github.com/openstack-ci/devstack-gate',
     license='Apache',
     author='Openstack CI team',
     author_email='openstack@lists.launchpad.net',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    cmdclass=common_setup.get_cmdclass(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
