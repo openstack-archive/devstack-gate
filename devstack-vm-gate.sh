9a0c27155bb32b5ae819d196a359d2f3077a820c
Julien Danjou <julien@danjou.info>
2013-09-24 15:24:59 +0200

    Enable Ceilometer alarm services
    Change-Id: I9415abaf189e3e5b6ed0204aaa29af0d8c09840a

--- devstack-vm-gate.sh
+++ devstack-vm-gate.sh
@@ -66,7 +66,7 @@ function setup_localrc() {
             MY_ENABLED_SERVICES=$MY_ENABLED_SERVICES,n-cell
         fi
     else # master
-        MY_ENABLED_SERVICES=$MY_ENABLED_SERVICES,s-proxy,s-account,s-container,s-object,cinder,c-api,c-vol,c-sch,c-bak,n-cond,heat,h-api,h-api-cfn,h-api-cw,h-eng,ceilometer-acompute,ceilometer-acentral,ceilometer-collector,ceilometer-api
+        MY_ENABLED_SERVICES=$MY_ENABLED_SERVICES,s-proxy,s-account,s-container,s-object,cinder,c-api,c-vol,c-sch,c-bak,n-cond,heat,h-api,h-api-cfn,h-api-cw,h-eng,ceilometer-acompute,ceilometer-acentral,ceilometer-collector,ceilometer-api,ceilometer-alarm-notifier,ceilometer-alarm-evaluator
         if [ "$DEVSTACK_GATE_NEUTRON" -eq "1" ]; then
             MY_ENABLED_SERVICES=$MY_ENABLED_SERVICES,quantum,q-svc,q-agt,q-dhcp,q-l3,q-meta,q-lbaas,q-vpn
             echo "Q_USE_DEBUG_COMMAND=True" >>localrc
