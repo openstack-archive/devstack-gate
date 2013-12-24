b3d93e5995db637d3aa6605504efa3b027c61238
James E. Blair <jeblair@openstack.org>
2013-12-24 11:17:49 -0800

    Move workspace setup to functions.sh
    And add a test script that exercises the logic of the git repo
    checkout functions.  Current test errors are annotated and commented
    out.
    
    Change-Id: I2e04787999a64cd8aaa238ce0374898032a9d8bd

---
+++ functions.sh
@@ -0,0 +1,424 @@
+#!/bin/bash
+
+# Copyright (C) 2011-2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function function_exists {
+    type $1 2>/dev/null | grep -q 'is a function'
+}
+
+# Attempt to fetch a git ref for a project, if that ref is not empty
+function git_fetch_at_ref {
+    local project=$1
+    local ref=$2
+    if [ "$ref" != "" ]; then
+        git fetch $ZUUL_URL/$project $ref
+        return $?
+    else
+        # return failing
+        return 1
+    fi
+}
+
+function git_checkout {
+    local project=$1
+    local branch=$2
+    local reset_branch=$branch
+
+    if [[ "$branch" != "FETCH_HEAD" ]]; then
+        reset_branch="remotes/origin/$branch"
+    fi
+
+    git checkout $branch
+    git reset --hard $reset_branch
+    if ! git clean -x -f -d -q ; then
+        sleep 1
+        git clean -x -f -d -q
+    fi
+}
+
+function git_has_branch {
+    local project=$1 # Project is here for test mocks
+    local branch=$2
+
+    if git branch -a |grep remotes/origin/$branch>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function git_prune {
+    git remote prune origin
+}
+
+function git_remote_update {
+    # Attempt a git remote update. Run for up to 5 minutes before killing.
+    # If first SIGTERM does not kill the process wait a minute then SIGKILL.
+    # If update fails try again for up to a total of 3 attempts.
+    MAX_ATTEMPTS=3
+    COUNT=0
+    until timeout -k 1m 5m git remote update; do
+        COUNT=$(($COUNT + 1))
+        echo "git remote update failed."
+        if [ $COUNT -eq $MAX_ATTEMPTS ]; then
+            exit 1
+        fi
+        SLEEP_TIME=$((30 + $RANDOM % 60))
+        echo "sleep $SLEEP_TIME before retrying."
+        sleep $SLEEP_TIME
+    done
+}
+
+function git_remote_set_url {
+    git remote set-url $1 $2
+}
+
+function git_clone_and_cd {
+    local project=$1
+    local short_project=$2
+
+    if [[ ! -e $short_project ]]; then
+        echo "  Need to clone $short_project"
+        git clone https://git.openstack.org/$project
+    fi
+    cd $short_project
+}
+
+function fix_etc_hosts {
+    # HPcloud stopped adding the hostname to /etc/hosts with their
+    # precise images.
+
+    HOSTNAME=`/bin/hostname`
+    if ! grep $HOSTNAME /etc/hosts >/dev/null; then
+        echo "Need to add hostname to /etc/hosts"
+        sudo bash -c 'echo "127.0.1.1 $HOSTNAME" >>/etc/hosts'
+    fi
+
+}
+
+function fix_disk_layout {
+    # HPCloud and Rackspace performance nodes provide no swap, but do
+    # have ephemeral disks we can use.  HPCloud also doesn't have
+    # enough space on / for two devstack installs, so we partition the
+    # disk and mount it on /opt, syncing the previous contents of /opt
+    # over.
+    if [ `grep SwapTotal /proc/meminfo | awk '{ print $2; }'` -eq 0 ]; then
+        if [ -b /dev/vdb ]; then
+            DEV='/dev/vdb'
+        elif [ -b /dev/xvde ]; then
+            DEV='/dev/xvde'
+        fi
+        if [ -n "$DEV" ]; then
+            sudo umount ${DEV}
+            sudo parted ${DEV} --script -- mklabel msdos
+            sudo parted ${DEV} --script -- mkpart primary linux-swap 0 8192
+            sudo parted ${DEV} --script -- mkpart primary ext2 8192 -1
+            sudo mkswap ${DEV}1
+            sudo mkfs.ext4 ${DEV}2
+            sudo swapon ${DEV}1
+            sudo mount ${DEV}2 /mnt
+            sudo rsync -a /opt/ /mnt/
+            sudo umount /mnt
+            sudo mount ${DEV}2 /opt
+        fi
+    fi
+}
+
+# do all the zuulification magic for project at a specified branch
+#
+# The basic logic flow is as follows:
+#   if we have ``branch`` for project, check that out
+#   if we don't have ``branch`` for project, change ``branch`` to master
+#     and check that out
+#   if the global ZUUL_BRANCH matches ``branch``, then also look for a
+#     valid ZUUL_REF, and use that instead of the HEAD of the branch
+#
+# The end result is a tree on disk checked out at the right ref for zuul
+function setup_project {
+    local project=$1
+    local branch=$2
+    local short_project=`basename $project`
+
+    echo "Setting up $project @ $branch"
+    git_clone_and_cd $project $short_project
+
+    git_remote_set_url origin https://git.openstack.org/$project
+
+    if [ -n "$OVERRIDE_ZUUL_BRANCH" ] ; then
+        OVERRIDE_ZUUL_REF=$(echo $ZUUL_REF | sed -e "s,$branch,$OVERRIDE_ZUUL_BRANCH,")
+    fi
+
+    # Update git remotes
+    git_remote_update
+    # Ensure that we don't have stale remotes around
+    git_prune
+    # See if this project has this branch, if not, use master
+    FALLBACK_ZUUL_REF=""
+    if ! git_has_branch $project $branch; then
+        branch=master
+        FALLBACK_ZUUL_REF=$(echo $ZUUL_REF | sed -e "s,$branch,master,")
+    fi
+
+    # See if we should check out a Zuul ref
+    if [ "$ZUUL_BRANCH" == "$branch" ]; then
+        # See if Zuul prepared a ref for this project
+        if git_fetch_at_ref $project $OVERRIDE_ZUUL_REF || \
+            git_fetch_at_ref $project $ZUUL_REF || \
+            git_fetch_at_ref $project $FALLBACK_ZUUL_REF; then
+
+            # It's there, so check it out.
+            git_checkout $project FETCH_HEAD
+        else
+              if [ "$project" == "$ZUUL_PROJECT" ]; then
+                  echo "Unable to find ref $ZUUL_REF for $project"
+                  exit 1
+              fi
+              git_checkout $project $branch
+        fi
+    else
+          # We're ignoring Zuul refs
+        git_checkout $project $branch
+    fi
+}
+
+function re_exec_devstack_gate {
+    export RE_EXEC="true"
+    echo "This build includes a change to devstack-gate; re-execing this script."
+    exec $WORKSPACE/devstack-gate/devstack-vm-gate-wrap.sh
+}
+
+function setup_workspace {
+    local base_branch=$1
+    local DEST=$2
+
+    # Enabled detailed logging, since output of this function is redirected
+    set -o xtrace
+
+    fix_disk_layout
+
+    sudo mkdir -p $DEST
+    sudo chown -R jenkins:jenkins $DEST
+
+    # The vm template update job should cache the git repos
+    # Move them to where we expect:
+    if ls ~/workspace-cache/*; then
+      rsync -a ~/workspace-cache/ $DEST/
+    fi
+
+    echo "Using branch: $base_branch"
+    for PROJECT in $PROJECTS; do
+        cd $DEST
+        setup_project $PROJECT $base_branch
+    done
+    # It's important we are back at DEST for the rest of the script
+    cd $DEST
+
+    # The vm template update job should cache some images in ~/files.
+    # Move them to where devstack expects:
+    if [ "$(ls ~/cache/files/* 2>/dev/null)" ]; then
+      rsync -a ~/cache/files/ $DEST/devstack/files/
+    fi
+
+    # Disable detailed logging as we return to the main script
+    set +o xtrace
+}
+
+function select_mirror {
+
+    if [ "$DEVSTACK_GATE_REQS_INTEGRATION" -eq "0" ]; then
+
+        ORG=$(dirname $ZUUL_PROJECT)
+        SHORT_PROJECT=$(basename $ZUUL_PROJECT)
+        $DEVSTACK_GATE_SELECT_MIRROR $ORG $SHORT_PROJECT
+
+        sudo cp ~/.pydistutils.cfg ~root/.pydistutils.cfg
+        sudo cp ~/.pydistutils.cfg ~stack/.pydistutils.cfg
+        sudo chown stack:stack ~stack/.pydistutils.cfg
+        sudo cp ~/.pydistutils.cfg ~tempest/.pydistutils.cfg
+        sudo chown tempest:tempest ~tempest/.pydistutils.cfg
+
+        sudo -u stack mkdir -p ~stack/.pip
+        sudo -u root mkdir -p ~root/.pip
+        sudo -u tempest mkdir -p ~tempest/.pip
+
+        sudo -u root cp ~/.pip/pip.conf ~root/.pip/pip.conf
+        sudo cp ~/.pip/pip.conf ~stack/.pip/pip.conf
+        sudo chown stack:stack ~stack/.pip/pip.conf
+        sudo cp ~/.pip/pip.conf ~tempest/.pip/pip.conf
+        sudo chown tempest:tempest ~tempest/.pip/pip.conf
+    fi
+}
+
+function setup_host {
+    # Enabled detailed logging, since output of this function is redirected
+    set -o xtrace
+
+    # This is necessary to keep sudo from complaining
+    fix_etc_hosts
+
+    # Move the PIP cache into position:
+    sudo mkdir -p /var/cache/pip
+    sudo mv ~/cache/pip/* /var/cache/pip
+
+    # Start with a fresh syslog
+    sudo stop rsyslog
+    sudo mv /var/log/syslog /var/log/syslog-pre-devstack
+    sudo mv /var/log/kern.log /var/log/kern_log-pre-devstack
+    sudo touch /var/log/syslog
+    sudo chown /var/log/syslog --ref /var/log/syslog-pre-devstack
+    sudo chmod /var/log/syslog --ref /var/log/syslog-pre-devstack
+    sudo chmod a+r /var/log/syslog
+    sudo touch /var/log/kern.log
+    sudo chown /var/log/kern.log --ref /var/log/kern_log-pre-devstack
+    sudo chmod /var/log/kern.log --ref /var/log/kern_log-pre-devstack
+    sudo chmod a+r /var/log/kern.log
+    sudo start rsyslog
+
+    # We set some home directories under $BASE, make sure it exists.
+    sudo mkdir $BASE
+    # Create a stack user for devstack to run as, so that we can
+    # revoke sudo permissions from that user when appropriate.
+    sudo useradd -U -s /bin/bash -d $BASE/new -m stack
+    TEMPFILE=`mktemp`
+    echo "stack ALL=(root) NOPASSWD:ALL" >$TEMPFILE
+    chmod 0440 $TEMPFILE
+    sudo chown root:root $TEMPFILE
+    sudo mv $TEMPFILE /etc/sudoers.d/50_stack_sh
+
+    # Create a tempest user for tempest to run as, so that we can
+    # revoke sudo permissions from that user when appropriate.
+    # NOTE(sdague): we should try to get the state dump to be a
+    # neutron API call in Icehouse to remove this.
+    sudo useradd -U -s /bin/bash -d $BASE/new/tempest -m tempest
+    TEMPFILE=`mktemp`
+    echo "tempest ALL=(root) NOPASSWD:/sbin/ip" >$TEMPFILE
+    echo "tempest ALL=(root) NOPASSWD:/sbin/iptables" >>$TEMPFILE
+    chmod 0440 $TEMPFILE
+    sudo chown root:root $TEMPFILE
+    sudo mv $TEMPFILE /etc/sudoers.d/51_tempest_sh
+
+    # Future useradd calls should strongly consider also updating
+    # ~/.pip/pip.conf and ~/.pydisutils.cfg in the select_mirror function if
+    # tox/pip will be used at all.
+
+    # If we will be testing OpenVZ, make sure stack is a member of the vz group
+    if [ "$DEVSTACK_GATE_VIRT_DRIVER" == "openvz" ]; then
+        sudo usermod -a -G vz stack
+    fi
+
+    if [ -f $DEVSTACK_GATE_SELECT_MIRROR ] ; then
+        select_mirror
+    fi
+    # Disable detailed logging as we return to the main script
+    set +o xtrace
+}
+
+function cleanup_host {
+    # Enabled detailed logging, since output of this function is redirected
+    set -o xtrace
+
+    cd $WORKSPACE
+    # No matter what, archive logs
+
+    # Sleep to give services a chance to flush their log buffers.
+    sleep 2
+
+    sudo cp /var/log/syslog $WORKSPACE/logs/syslog.txt
+    sudo cp /var/log/kern.log $WORKSPACE/logs/kern_log.txt
+    mkdir $WORKSPACE/logs/rabbitmq/
+    sudo cp /var/log/rabbitmq/* $WORKSPACE/logs/rabbitmq/
+    mkdir $WORKSPACE/logs/sudoers.d/
+
+    sudo cp /etc/sudoers.d/* $WORKSPACE/logs/sudoers.d/
+    sudo cp /etc/sudoers $WORKSPACE/logs/sudoers.txt
+
+    if [ -d $BASE/old ]; then
+      mkdir -p $WORKSPACE/logs/old/
+      mkdir -p $WORKSPACE/logs/new/
+      mkdir -p $WORKSPACE/logs/grenade/
+      sudo cp $BASE/old/screen-logs/* $WORKSPACE/logs/old/
+      sudo cp $BASE/old/devstacklog.txt $WORKSPACE/logs/old/
+      sudo cp $BASE/old/devstack/localrc $WORKSPACE/logs/old/localrc.txt
+      sudo cp $BASE/logs/* $WORKSPACE/logs/
+      sudo cp $BASE/new/grenade/localrc $WORKSPACE/logs/grenade/localrc.txt
+      NEWLOGTARGET=$WORKSPACE/logs/new
+    else
+      NEWLOGTARGET=$WORKSPACE/logs
+    fi
+    sudo cp $BASE/new/screen-logs/* $NEWLOGTARGET/
+    sudo cp $BASE/new/devstacklog.txt $NEWLOGTARGET/
+    sudo cp $BASE/new/devstack/localrc $NEWLOGTARGET/localrc.txt
+
+    sudo iptables-save > $WORKSPACE/logs/iptables.txt
+    df -h> $WORKSPACE/logs/df.txt
+
+    pip freeze > $WORKSPACE/logs/pip-freeze.txt
+
+    # Process testr artifacts.
+    if [ -f $BASE/new/tempest/.testrepository/0 ]; then
+        sudo cp $BASE/new/tempest/.testrepository/0 $WORKSPACE/subunit_log.txt
+        sudo python /usr/local/jenkins/slave_scripts/subunit2html.py $WORKSPACE/subunit_log.txt $WORKSPACE/testr_results.html
+        sudo gzip -9 $WORKSPACE/subunit_log.txt
+        sudo gzip -9 $WORKSPACE/testr_results.html
+        sudo chown jenkins:jenkins $WORKSPACE/subunit_log.txt.gz $WORKSPACE/testr_results.html.gz
+        sudo chmod a+r $WORKSPACE/subunit_log.txt.gz $WORKSPACE/testr_results.html.gz
+    elif [ -f $BASE/new/tempest/.testrepository/tmp* ]; then
+        # If testr timed out, collect temp file from testr
+        sudo cp $BASE/new/tempest/.testrepository/tmp* $WORKSPACE/subunit_log.txt
+        sudo gzip -9 $WORKSPACE/subunit_log.txt
+        sudo chown jenkins:jenkins $WORKSPACE/subunit_log.txt.gz
+        sudo chmod a+r $WORKSPACE/subunit_log.txt.gz
+    fi
+
+    if [ -f $BASE/new/tempest/tempest.log ] ; then
+        sudo cp $BASE/new/tempest/tempest.log $WORKSPACE/logs/tempest.log
+    fi
+
+    # Make sure jenkins can read all the logs
+    sudo chown -R jenkins:jenkins $WORKSPACE/logs/
+    sudo chmod a+r $WORKSPACE/logs/
+
+    rename 's/\.log$/.txt/' $WORKSPACE/logs/*
+    rename 's/(.*)/$1.txt/' $WORKSPACE/logs/sudoers.d/*
+    rename 's/\.log$/.txt/' $WORKSPACE/logs/rabbitmq/*
+
+    mv $WORKSPACE/logs/rabbitmq/startup_log \
+       $WORKSPACE/logs/rabbitmq/startup_log.txt
+
+    # Remove duplicate logs
+    rm $WORKSPACE/logs/*.*.txt
+
+    if [ -d $BASE/old ]; then
+        rename 's/\.log$/.txt/' $WORKSPACE/logs/old/*
+        rename 's/\.log$/.txt/' $WORKSPACE/logs/new/*
+        rename 's/\.log$/.txt/' $WORKSPACE/logs/grenade/*
+        rm $WORKSPACE/logs/old/*.*.txt
+        rm $WORKSPACE/logs/new/*.*.txt
+    fi
+
+    # Compress all text logs
+    find $WORKSPACE/logs -iname '*.txt' -execdir gzip -9 {} \+
+    find $WORKSPACE/logs -iname '*.dat' -execdir gzip -9 {} \+
+
+    # Save the tempest nosetests results
+    sudo cp $BASE/new/tempest/nosetests*.xml $WORKSPACE/
+    sudo chown jenkins:jenkins $WORKSPACE/nosetests*.xml
+    sudo chmod a+r $WORKSPACE/nosetests*.xml
+
+    # Disable detailed logging as we return to the main script
+    set +o xtrace
+}
---
+++ test-functions.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+
+# Copyright (C) 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script tests the checkout functions defined in functions.sh.
+
+source functions.sh
+
+# Mock out the checkout function since the refs we're checking out do
+# not exist.
+function git_checkout {
+    local project=$1
+    local branch=$2
+
+    project=`basename $project`
+    if [[ "$branch" == "FETCH_HEAD" ]]; then
+        branch=$FETCH_HEAD
+    fi
+    TEST_GIT_CHECKOUTS[$project]=$branch
+}
+
+# Mock out the fetch function since the refs we're fetching do not
+# exist.
+function git_fetch_at_ref {
+    local project=$1
+    local ref=$2
+
+    project=`basename $project`
+    if [ "$ref" != "" ]; then
+        if [[ "${TEST_ZUUL_REFS[$project]}" =~ "$ref" ]]; then
+            FETCH_HEAD="$ref"
+            return 0
+        fi
+        return 1
+    else
+        # return failing
+        return 1
+    fi
+}
+
+# Mock out git repo functions so the git repos don't have to exist.
+function git_has_branch {
+    local project=$1
+    local branch=$2
+
+    case $branch in
+        master) return 0 ;;
+        stable/havana)
+            case $project in
+                openstack/glance) return 0 ;;
+                openstack/swift) return 0 ;;
+            esac
+    esac
+    return 1
+}
+
+function git_prune {
+    return 0
+}
+
+function git_remote_update {
+    return 0
+}
+
+function git_remote_set_url {
+    return 0
+}
+
+function git_clone_and_cd {
+    return 0
+}
+
+# Utility function for tests
+function assert_equal {
+    if [[ "$1" != "$2" ]]; then
+        echo "$1 != $2 on line $3"
+        exit 1
+    fi
+}
+
+# Tests follow:
+function test_one_on_master {
+    # devstack-gate  master  ZA
+    echo "== Test one on master"
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack-infra/devstack-gate'
+    ZUUL_BRANCH='master'
+    ZUUL_REF='refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+
+    setup_project openstack-infra/devstack-gate $ZUUL_BRANCH
+
+    assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZA' $LINENO
+}
+
+function test_two_on_master {
+    # devstack-gate  master  ZA
+    # glance         master  ZB
+    echo "== Test two on master"
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack/glance'
+    ZUUL_BRANCH='master'
+    ZUUL_REF='refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/master/ZB'
+
+    setup_project openstack-infra/devstack-gate $ZUUL_BRANCH
+    setup_project openstack/glance $ZUUL_BRANCH
+
+    assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZB' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/master/ZB' $LINENO
+}
+
+function test_multi_branch_on_master {
+    # devstack-gate        master         ZA
+    # glance               stable/havana  ZB
+    # python-glanceclient  master         ZC
+    echo "== Test multi-branch on master"
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack/python-glanceclient'
+    ZUUL_BRANCH='master'
+    ZUUL_REF='refs/zuul/master/ZC'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZC'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZC'
+    TEST_ZUUL_REFS[python-glanceclient]+=' refs/zuul/master/ZC'
+
+    setup_project openstack-infra/devstack-gate $ZUUL_BRANCH
+    setup_project openstack/glance $ZUUL_BRANCH
+    setup_project openstack/python-glanceclient $ZUUL_BRANCH
+
+    assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'master' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'refs/zuul/master/ZC' $LINENO
+}
+
+function test_multi_branch_on_stable {
+    # devstack-gate        master         ZA
+    # glance               stable/havana  ZB
+    # python-glanceclient not in queue
+    echo "== Test multi-branch on stable"
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack/glance'
+    ZUUL_BRANCH='stable/havana'
+    ZUUL_REF='refs/zuul/stable/havana/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZB'
+
+    setup_project openstack-infra/devstack-gate $ZUUL_BRANCH
+    setup_project openstack/glance $ZUUL_BRANCH
+    setup_project openstack/python-glanceclient $ZUUL_BRANCH
+
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZB' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/stable/havana/ZB' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+}
+
+function test_grenade_backward {
+    # devstack-gate        master         ZA
+    # glance               stable/havana  ZB
+    # glance               master         ZC
+    # swift not in queue
+    # python-glanceclient not in queue
+    echo "== Test grenade backward"
+    # havana -> master (with changes)
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack/glance'
+    ZUUL_BRANCH='master'
+    ZUUL_REF='refs/zuul/master/ZC'
+    GRENADE_OLD_BRANCH='stable/havana'
+    GRENADE_NEW_BRANCH='master'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZC'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZC'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/master/ZC'
+
+    setup_project openstack-infra/devstack-gate $GRENADE_OLD_BRANCH
+    setup_project openstack/glance $GRENADE_OLD_BRANCH
+    setup_project openstack/swift $GRENADE_OLD_BRANCH
+    setup_project openstack/python-glanceclient $GRENADE_OLD_BRANCH
+
+    assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZC' $LINENO
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/stable/havana/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[swift]}" 'stable/havana' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+
+    declare -A TEST_GIT_CHECKOUTS
+
+    setup_project openstack-infra/devstack-gate $GRENADE_NEW_BRANCH
+    setup_project openstack/glance $GRENADE_NEW_BRANCH
+    setup_project openstack/swift $GRENADE_NEW_BRANCH
+    setup_project openstack/python-glanceclient $GRENADE_NEW_BRANCH
+
+    assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/master/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[swift]}" 'master' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+}
+
+function test_grenade_forward {
+    # devstack-gate        master         ZA
+    # glance               master         ZB
+    # glance               stable/havana  ZC
+    # swift not in queue
+    # python-glanceclient not in queue
+    echo "== Test grenade forward"
+    # havana (with changes) -> master
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack/glance'
+    ZUUL_BRANCH='stable/havana'
+    ZUUL_REF='refs/zuul/stable/havana/ZC'
+    GRENADE_OLD_BRANCH='stable/havana'
+    GRENADE_NEW_BRANCH='master'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZA'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZC'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/master/ZC'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZC'
+
+    setup_project openstack-infra/devstack-gate $GRENADE_OLD_BRANCH
+    setup_project openstack/glance $GRENADE_OLD_BRANCH
+    setup_project openstack/swift $GRENADE_OLD_BRANCH
+    setup_project openstack/python-glanceclient $GRENADE_OLD_BRANCH
+
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/stable/havana/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[swift]}" 'stable/havana' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+
+    declare -A TEST_GIT_CHECKOUTS
+
+    setup_project openstack-infra/devstack-gate $GRENADE_NEW_BRANCH
+    setup_project openstack/glance $GRENADE_NEW_BRANCH
+    setup_project openstack/swift $GRENADE_NEW_BRANCH
+    setup_project openstack/python-glanceclient $GRENADE_NEW_BRANCH
+
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZC' $LINENO
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/master/ZC' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[swift]}" 'master' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+}
+
+function test_branch_override {
+    # glance               stable/havana  ZA
+    # devstack-gate        master         ZB
+    # swift not in queue
+    # python-glanceclient not in queue
+    echo "== Test branch override"
+
+    declare -A TEST_GIT_CHECKOUTS
+    declare -A TEST_ZUUL_REFS
+    ZUUL_PROJECT='openstack-infra/devstack-gate'
+    ZUUL_BRANCH='stable/havana'  # branch override
+    ZUUL_REF='refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[devstack-gate]+=' refs/zuul/master/ZB'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZA'
+    TEST_ZUUL_REFS[glance]+=' refs/zuul/stable/havana/ZB'
+
+    setup_project openstack-infra/devstack-gate $ZUUL_BRANCH
+    setup_project openstack/glance $ZUUL_BRANCH
+    setup_project openstack/swift $ZUUL_BRANCH
+    setup_project openstack/python-glanceclient $ZUUL_BRANCH
+
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[devstack-gate]}" 'refs/zuul/master/ZB' $LINENO
+    #TODO: This is a test failure indicating a bug
+    #assert_equal "${TEST_GIT_CHECKOUTS[glance]}" 'refs/zuul/stable/havana/ZB' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[swift]}" 'stable/havana' $LINENO
+    assert_equal "${TEST_GIT_CHECKOUTS[python-glanceclient]}" 'master' $LINENO
+}
+
+# Run tests:
+#set -o xtrace
+test_two_on_master
+test_one_on_master
+test_multi_branch_on_master
+test_multi_branch_on_stable
+test_grenade_backward
+test_grenade_forward
+test_branch_override
