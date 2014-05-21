6bd3b4a1fffc697eaae54f79c9f98df82133e186
Sean Dague <sean@dague.net>
2014-05-21 11:26:09 -0400

    fix grenade branch selection
    the grenade branch selection ignored the ability to do branch
    override in jobs, as such wasn't correctly enforcing things on
    branchless tempest. Use OVERRIDE_ZUUL_BRANCH earlier in grenade
    branch selection to prevent us from landing tempest tests that
    will break grenade upgrades on stable/icehouse.
    
    Change-Id: I55fa11f685fb33580835ffbad34eb7fb140f7e75

--- devstack-vm-gate-wrap.sh
+++ devstack-vm-gate-wrap.sh
@@ -204,9 +204,12 @@ export DEVSTACK_GATE_GRENADE_FORWARD=${DEVSTACK_GATE_GRENADE_FORWARD:-0}
 # DGGPN=1 means upgrade everything but n-cpu.
 export DEVSTACK_GATE_GRENADE_PARTIAL_NCPU=${DEVSTACK_GATE_GRENADE_PARTIAL_NCPU:-0}
 
+# the branch name for selecting grenade branches
+GRENADE_BASE_BRANCH=${OVERRIDE_ZUUL_BRANCH:-${ZUUL_BRANCH}}
+
 if [ "$DEVSTACK_GATE_GRENADE" -eq "1" ]; then
     export DEVSTACK_GATE_TEMPEST=1
-    if [ "$ZUUL_BRANCH" == "stable/icehouse" ]; then
+    if [ "$GRENADE_BASE_BRANCH" == "stable/icehouse" ]; then
         export GRENADE_OLD_BRANCH="stable/havana"
         export GRENADE_NEW_BRANCH="stable/icehouse"
     else # master
@@ -221,10 +224,10 @@ if [ "$DEVSTACK_GATE_GRENADE" -eq "1" ]; then
     # the roll forward case
 elif [ "$DEVSTACK_GATE_GRENADE_FORWARD" -eq "1" ]; then
     export DEVSTACK_GATE_TEMPEST=1
-    if [ "$ZUUL_BRANCH" == "stable/havana" ]; then
+    if [ "$GRENADE_BASE_BRANCH" == "stable/havana" ]; then
         export GRENADE_OLD_BRANCH="stable/havana"
         export GRENADE_NEW_BRANCH="stable/icehouse"
-    elif [ "$ZUUL_BRANCH" == "stable/icehouse" ]; then
+    elif [ "$GRENADE_BASE_BRANCH" == "stable/icehouse" ]; then
         export GRENADE_OLD_BRANCH="stable/icehouse"
         export GRENADE_NEW_BRANCH="master"
     fi
