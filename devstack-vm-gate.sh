f42d6c8a8d39cfc65fb1f5804f84608a0e7eab59
Chris Dent <chdent@redhat.com>
2015-06-01 09:39:11 +0000

    Disable mod wsgi for ceilometer in grenade
    Until there is more intelligent upgrading (which will come when
    ceilometer is moved to a grenade plugin) upgrades of the ceilometer
    api service between branches are unpredictable if mod wsgi is in
    place for just one side.
    
    Change-Id: Ib38177125ddc8e97e74237e9242e2ea09cff28d4

--- devstack-vm-gate.sh
+++ devstack-vm-gate.sh
@@ -318,13 +318,17 @@ EOF
         else
             echo "GRENADE_PHASE=target" >> "$localrc_file"
         fi
-        # keystone deployed with mod wsgi cannot be upgraded or migrated
+        # services deployed with mod wsgi cannot be upgraded or migrated
         # until https://launchpad.net/bugs/1365105 is resolved.
         case $GRENADE_BASE_BRANCH in
             "stable/icehouse")
                 ;&
             "stable/juno")
                 echo "KEYSTONE_USE_MOD_WSGI=False" >> "$localrc_file"
+                echo "CEILOMETER_USE_MOD_WSGI=False" >> "$localrc_file"
+                ;;
+            "stable/kilo")
+                echo "CEILOMETER_USE_MOD_WSGI=False" >> "$localrc_file"
                 ;;
         esac
     fi
